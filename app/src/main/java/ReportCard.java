2c5dd19ffd1c072d839067aac691bf1db67e6c08
brunobpr <brunopr.bpr@gmail.com>
2017-05-29 14:28:12 +0100

    29/05/2017

--- app/src/main/java/ReportCard.java
+++ app/src/main/java/ReportCard.java
@@ -17,30 +17,81 @@ public class ReportCard {
         mStudentIdentity = studentIdentity;
         mCurrentYear = currentYear;
         mPortugueseGrade = portugueseGrade;
-        mMathsGrade  = mathsGrade;
+        mMathsGrade = mathsGrade;
         mEnglishGrade = englishGrade;
         mGeographyGrade = geographyGrade;
         mHistoryGrade = historyGrade;
     }
 
     //Getting student and date
-    public int getStudentIdentity(){return mStudentIdentity;}
-    public int getCurrentYear(){return mCurrentYear;}
+    public int getStudentIdentity() {
+        return mStudentIdentity;
+    }
+
+    public void setStudentIdentity(int studentIdentity) {
+        mStudentIdentity = studentIdentity;
+    }
+
+    //Setting student and date
+
+    public int getCurrentYear() {
+        return mCurrentYear;
+    }
+
+    public void setCurrentYear(int currentYear) {
+        mCurrentYear = currentYear;
+    }
+
     //Getting grades of the student
-    public String getPortugueseGrade(){return mPortugueseGrade;}
-    public String getMathsGrade() {return mMathsGrade;}
-    public String getEnglishGrade() {return mEnglishGrade;}
-    public String getGeographyGrade() {return mGeographyGrade;}
-    public String getHistoryGrade(){return mHistoryGrade;}
+    public String getPortugueseGrade() {
+        return mPortugueseGrade;
+    }
+
+    //Setting grade of the student
+    public void setPortugueseGrade(String portugueseGrade) {
+        mPortugueseGrade = portugueseGrade;
+    }
+
+    public String getMathsGrade() {
+        return mMathsGrade;
+    }
+
+    public void setMathsGrade(String mathsGrade) {
+        mMathsGrade = mathsGrade;
+    }
+
+    public String getEnglishGrade() {
+        return mEnglishGrade;
+    }
+
+    public void setEnglishGrade(String englishGrade) {
+        mEnglishGrade = englishGrade;
+    }
+
+    public String getGeographyGrade() {
+        return mGeographyGrade;
+    }
+
+    public void setGeographyGrade(String geographyGrade) {
+        mGeographyGrade = geographyGrade;
+    }
+
+    public String getHistoryGrade() {
+        return mHistoryGrade;
+    }
+
+    public void setHistoryGrade(String historyGrade) {
+        mHistoryGrade = historyGrade;
+    }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "Student ID:" + mStudentIdentity +
                 "\nGrades from: " + mCurrentYear +
                 "\nPortuguese: " + mPortugueseGrade +
                 "\nMathematics: " + mMathsGrade +
                 "\nEnglish: " + mEnglishGrade +
-                "\nGeography: " + mGeographyGrade+
+                "\nGeography: " + mGeographyGrade +
                 "\nHistory: " + mHistoryGrade;
     }
 }
